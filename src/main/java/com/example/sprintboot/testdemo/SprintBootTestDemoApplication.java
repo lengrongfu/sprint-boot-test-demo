bcd7d58f04c42cca27028b607310dc27b1cb99ea
lengrongfu <lenronfu@gmail.com>
2021-09-05 17:20:19 +0800

    init

---
+++ src/main/java/com/example/sprintboot/testdemo/SprintBootTestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprintboot.testdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootTestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootTestDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sprintboot/testdemo/SprintBootTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintboot.testdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootTestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
